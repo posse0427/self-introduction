c3a1570cab09de728c417db77c3a9613f45bbc6f
posse0427 <posse.kirby@gmail.com>
2020-02-18 21:32:12 +0900

    feat: Twitterのツイートデータをサイトに表示できるようにした

--- src/components/others.tsx
+++ src/components/others.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Timeline, Follow } from "react-twitter-widgets";
 
 const others: React.FC = () => {
   const title: string = "Others";
@@ -7,6 +8,17 @@ const others: React.FC = () => {
     <div>
       <h1>{title}</h1>
       {description}
+      <Timeline
+        dataSource={{
+          sourceType: "profile",
+          screenName: "posse0427"
+        }}
+        options={{
+          username: "posse0427",
+          height: "600"
+        }}
+      />
+      <Follow username="posse0427" />
     </div>
   );
 };
