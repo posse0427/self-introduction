b2f33e9dc336c0c539fae7826475212729c92c29
posse0427 <posse.kirby@gmail.com>
2020-03-01 15:31:41 +0900

    feat: Material-UIの機能を入れた。また、後々消す予定だが、styled-componentsによってボタンの設定を変えた。

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -7,6 +7,20 @@ import about from "./components/about";
 import skills from "./components/skills";
 import works from "./components/works";
 import others from "./components/others";
+import styled from "styled-components";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Button, Grid } from "@material-ui/core";
+
+// Material-UI をカッコで囲んで、styled の引数にしてやる
+const StyledButton = styled(Button)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 3px;
+  border: 0;
+  color: white;
+  height: 48px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+`;
 
 type Props = {
   children: any;
@@ -17,54 +31,80 @@ type Prop = {
   location: any;
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
+
 const notFound = () => (
   <div>
     <h1>404</h1>存在しないページです
   </div>
 );
 
-const MenuBar: React.FC<Props> = ({ children }) => (
-  <div>
-    <ul style={{ display: "flex" }}>
-      <li>
-        <Link to="/">top</Link>
-      </li>
-      <li>
-        <Link to="/about">about</Link>
-      </li>
-      <li>
-        <Link to="/works">works</Link>
-      </li>
-      <li>
-        <Link to="/skills">skills</Link>
-      </li>
-      <li>
-        <Link to="/others">others</Link>
-      </li>
-    </ul>
-    {children}
-  </div>
-);
+const MenuBar: React.FC<Props> = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Grid container alignItems="center" justify="center">
+              <Link to="/">
+                <StyledButton>top</StyledButton>
+              </Link>
+              <Link to="/about">
+                <StyledButton>about</StyledButton>
+              </Link>
+              <Link to="/works">
+                <StyledButton>works</StyledButton>
+              </Link>
+              <Link to="/skills">
+                <StyledButton>skills</StyledButton>
+              </Link>
+              <Link to="/others">
+                <StyledButton>others</StyledButton>
+              </Link>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </div>
+      {children}
+    </div>
+  );
+};
 
 const Menu: React.FC<Prop> = ({ location }) => {
   const currentkey = location.pathname.split("/")[1] || "";
 
   return (
     <MenuBar style={{ width: "500px", textAlign: "left" }}>
-      <TransitionGroup>
-        <CSSTransition key={currentkey} classNames="fade" timeout={800}>
-          <div style={{ marginLeft: "50px", position: "absolute" }}>
-            <Switch location={location}>
-              <Route path="/" exact component={top} />
-              <Route path="/about" exact component={about} />
-              <Route path="/works" exact component={works} />
-              <Route path="/skills" exact component={skills} />
-              <Route path="/others" exact component={others} />
-              <Route exact component={notFound} />
-            </Switch>
-          </div>
-        </CSSTransition>
-      </TransitionGroup>
+      <Grid container alignItems="center" justify="center">
+        <TransitionGroup>
+          <CSSTransition key={currentkey} classNames="fade" timeout={800}>
+            <div style={{ marginLeft: "50px", position: "absolute" }}>
+              <Switch location={location}>
+                <Route path="/" exact component={top} />
+                <Route path="/about" exact component={about} />
+                <Route path="/works" exact component={works} />
+                <Route path="/skills" exact component={skills} />
+                <Route path="/others" exact component={others} />
+                <Route exact component={notFound} />
+              </Switch>
+            </div>
+          </CSSTransition>
+        </TransitionGroup>
+      </Grid>
     </MenuBar>
   );
 };
