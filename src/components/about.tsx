59ce95ba20216885817534698fa0051fe5ec2d39
posse0427 <posse.kirby@gmail.com>
2020-03-01 22:48:08 +0900

    feat: デザイン面を強化した

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,38 +1,65 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia
+} from "@material-ui/core";
 
-const about: React.FC = () => {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
   const title: string = "自己紹介";
+
   return (
     <div>
       <h1>{title}</h1>
-      <table>
-        <tr>
-          <th>nickname</th>
-          <td>暗号名ポセイドン</td>
-        </tr>
-        <tr>
-          <th>GitHub</th>
-          <td>
-            <a href="https://github.com/posse0427">posse0427</a>
-          </td>
-        </tr>
-        <tr>
-          <th>Twitter</th>
-          <td>
-            <a href="https://twitter.com/posse0427">@posse0427</a>
-          </td>
-        </tr>
-        <tr>
-          <th>Youtube</th>
-          <td>
-            <a href="https://www.youtube.com/channel/UCtPnwsb-6cbcW93ac5AlMHg">
-              暗号名ポセイドン
-            </a>
-          </td>
-        </tr>
-      </table>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardContent>
+            <CardMedia
+              className={classes.media}
+              image="/src/自己紹介.jpeg"
+              title="Contemplative Reptile"
+            />
+          </CardContent>
+        </CardActionArea>
+        <table>
+          <tr>
+            <th>nickname</th>
+            <td>暗号名ポセイドン</td>
+          </tr>
+          <tr>
+            <th>GitHub</th>
+            <td>
+              <a href="https://github.com/posse0427">posse0427</a>
+            </td>
+          </tr>
+          <tr>
+            <th>Twitter</th>
+            <td>
+              <a href="https://twitter.com/posse0427">@posse0427</a>
+            </td>
+          </tr>
+          <tr>
+            <th>Youtube</th>
+            <td>
+              <a href="https://www.youtube.com/channel/UCtPnwsb-6cbcW93ac5AlMHg">
+                暗号名ポセイドン
+              </a>
+            </td>
+          </tr>
+        </table>
+      </Card>
     </div>
   );
-};
-
-export default about;
+}
--- src/components/works.tsx
+++ src/components/works.tsx
@@ -1,14 +1,49 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  CardActions,
+  Button,
+  CardContent,
+  Typography
+} from "@material-ui/core";
 
-const works: React.FC = () => {
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
+
+export default function SimpleCard() {
+  const classes = useStyles();
   const title: string = "Works";
   const description: string = "2枚目のページです";
+
   return (
     <div>
       <h1>{title}</h1>
       {description}
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h2" component="h2">
+            be
+          </Typography>
+          <Typography variant="body2" component="p">
+            well meaning and kindly.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">
+            <a href="https://posseblog.herokuapp.com/">URL</a>
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
-};
-
-export default works;
+}
